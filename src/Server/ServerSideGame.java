be61af91fcccd046ca3a597fd3951a8f46d00c8e
Meron Lindkvist <meron.hagos2@gmail.com>
2020-11-26 21:13:07 +0100

    Försöker hantera player två också.

--- src/Server/ServerSideGame.java
+++ src/Server/ServerSideGame.java
@@ -21,41 +21,50 @@ public class ServerSideGame extends Thread{
     public ServerSideGame(DAO dao){
         this.dao = dao;
     }
+     // clienthandler som parameter för att veta vilken spelare som har skickat objectet(input)
+    public void game(Object input, ClientHandler player){
 
-    public void game(Object input){
-
-        player1.sendMessage("Hejsan");
-        player2.sendMessage("Avvakta medans player1 väljer kategori");
+        //player1.sendMessage("Välj kategori");
+        //player2.sendMessage("Avvakta medans player1 väljer kategori");
 
         if(((String)input).equals("Geografi")){
             question = dao.g1;
-            dao.handleQuestion((String)input);
             player1.sendMessage(question);
+            player2.sendMessage(question);//skickar frågan till player två också
         }
         else if(((String)input).equals("Nöje")){
             question = dao.p1;
             player1.sendMessage(question);
+            player2.sendMessage(question);
         }
         else if(((String)input).equals("Sport")){
             question = dao.s1;
             player1.sendMessage(question);
+            player2.sendMessage(question);
         }
         else if(((String)input).equals("Matematik")){
             question = dao.m1;
             player1.sendMessage(question);
+            player2.sendMessage(question);
         }
+        //kollar vilken spelare som skickade svaret.
         else if (((String)input).equals(question.getAnswer())){
-            player1.sendMessage("Svaret är korrekt! " + input);
-            //player2.sendMessage("Svaret är korrekt! " + input);
-            player1.sendMessage("End of game");
-            //player2.sendMessage("End of game");
+            if(player == player1) {
+                player1.sendMessage("Svaret är korrekt! " + input);
+                player1.sendMessage("End of game");
+            } else {
+                player2.sendMessage("Svaret är korrekt! " + input);
+                player2.sendMessage("End of game");
+            }
         } else {
-            player1.sendMessage("Svaret är fel! " + input);
-           // player2.sendMessage("Svaret är fel! " + input);
-            player1.sendMessage("End of game");
-            //player2.sendMessage("End of game");
+            if(player == player1) {
+                player1.sendMessage("Svaret är fel! " + input);
+                player1.sendMessage("End of game");
+            } else {
+                player2.sendMessage("Svaret är fel! " + input);
+                player2.sendMessage("End of game");
+            }
         }
-
     }
 
     public void setPlayer1(ClientHandler player1) {
@@ -64,5 +73,13 @@ public class ServerSideGame extends Thread{
 
     public void setPlayer2(ClientHandler player2) {
         this.player2 = player2;
+    }
+// add setReady metod för att skicka första medelandet när de är redo.
+    public void setReady(ClientHandler player) {
+        if(player == player1) {
+            player1.sendMessage("Välj kategori");
+        } else {
+            player2.sendMessage("Avvakta medans player1 väljer kategori");
+        }
     }
 }
